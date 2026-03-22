e4ac666406cc84ec25ba426122769b908778ea72
Matheus Monte <118563754+matheusmontev@users.noreply.github.com>
2026-03-22 19:17:50 -0300

    Update user list in usuarios.js
    Removed some users and updated the username for Matheus.

--- js/usuarios.js
+++ js/usuarios.js
@@ -3,28 +3,13 @@
  */
 const LISTA_DE_USUARIOS = [
     {
-        "usuario": "admin",
-        "senha": "123",
-        "link_de_redirecionamento": "https://google.com"
-    },
-    {
         "usuario": "ELOISA",
         "senha": "131427",
         "link_de_redirecionamento": "https://andreliffit.my.canva.site/andre"
     },
     {
-        "usuario": "visitante",
-        "senha": "000",
-        "link_de_redirecionamento": "https://example.com"
-    },
-    {
-        "usuario": "matheus",
+        "usuario": "Matheus",
         "senha": "123",
-        "link_de_redirecionamento": "https://www.instagram.com/matheusmontev/"
-    },
-    {
-        "usuario": "123",
-        "senha": "123",
-        "link_de_redirecionamento": "https://github.com/matheusmontev/saas-de-treino-Andr-"
+        "link_de_redirecionamento": "https://aluno.uniftc.edu.br/#/home"
     }
 ];
