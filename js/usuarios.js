35c3f81123cc91bb3cc7137bee057ec24d246939
AndreLiffit <3877317al@gmail.com>
2026-03-23 17:57:06 -0300

    Add user 'maycon' with redirect link
    Added a new user with a redirect link to Canva.

--- js/usuarios.js
+++ js/usuarios.js
@@ -11,5 +11,10 @@ const LISTA_DE_USUARIOS = [
         "usuario": "Matheus",
         "senha": "123",
         "link_de_redirecionamento": "https://aluno.uniftc.edu.br/#/home"
+    },
+    {
+        "usuario": "maycon",
+        "senha": "12345",
+        "link_de_redirecionamento": "https://www.canva.com/design/DAHEyIyO6mg/wDVP4jaGQI2sAgXKgZE9Bg/edit?utm_content=DAHEyIyO6mg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
     }
 ];
