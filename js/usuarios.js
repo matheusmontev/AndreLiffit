b2aa3850b67bf9519c96c2912972cb3cd4d92016
AndreLiffit <3877317al@gmail.com>
2026-04-07 16:20:20 -0300

    Delete user 'Matheus' from usuarios.js
    Removed user 'Matheus' from the user list.

--- js/usuarios.js
+++ js/usuarios.js
@@ -18,11 +18,6 @@ const LISTA_DE_USUARIOS = [
         "link_de_redirecionamento": "https://andreliffit.my.canva.site/andre"
     },
     {
-        "usuario": "Matheus",
-        "senha": "123",
-        "link_de_redirecionamento": "https://aluno.uniftc.edu.br/#/home"
-    },
-    {
         "usuario": "maycon",
         "senha": "12345",
         "link_de_redirecionamento": "https://andreliffit.my.canva.site/maycon"
